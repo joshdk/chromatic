9cf3ee74bdd2070b578b7efff4db52d8592e1f67
Josh Komoroske <jdkomo@gmail.com>
2018-08-06 09:35:19 -0700

    Command line flags that print version and help information (#29)

--- main.go
+++ main.go
@@ -8,19 +8,24 @@ import (
 	"fmt"
 	"os"
 
+	"flag"
+
 	"github.com/joshdk/chromatic/chromatic"
 )
 
-func mainFunc(args []string) error {
-	configFile := "chromatic.yml"
-	if len(args) >= 2 {
-		configFile = args[1]
-	}
-	return chromatic.Run(configFile)
-}
+var (
+	version = "development"
+)
 
 func main() {
-	if err := mainFunc(os.Args); err != nil {
+	versionFlag := flag.Bool("version", false, "Displays the version and exits.")
+	flag.Parse()
+
+	if *versionFlag == true {
+		fmt.Println(version)
+		os.Exit(2)
+
+	} else if err := mainFunc(flag.Arg(0)); err != nil {
 		fmt.Fprintf(os.Stderr, "chromatic: %s\n", err.Error())
 		switch err {
 		case chromatic.ErrorTimeoutExceeded:
@@ -29,4 +34,11 @@ func main() {
 			os.Exit(1)
 		}
 	}
+}
+
+func mainFunc(configFile string) error {
+	if configFile == "" {
+		configFile = "chromatic.yml"
+	}
+	return chromatic.Run(configFile)
 }
